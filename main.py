a57d0c15055c05da61df0ef7f48c193845b41796
ijcruic <iain.j.cruickshank@gmail.com>
2018-04-13 10:06:12 -0400

    updated text

--- main.py
+++ main.py
@@ -37,11 +37,6 @@ def about():
     template = JINJA_ENVIRONMENT.get_template('templates/about.html')
     return template.render()
 
-@app.route('/quality')
-def quality():
-    template = JINJA_ENVIRONMENT.get_template('templates/quality.html')
-    return template.render()
-
 @app.errorhandler(404)
 def page_not_found(e):
     """Return a custom 404 error."""
